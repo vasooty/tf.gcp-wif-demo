421601d9dc9f219d3258cb15b6d89a30c9dc68f8
vasooty <47421689+vasooty@users.noreply.github.com>
2025-07-26 18:12:53 +0530

    Update provide.tf

--- provide.tf
+++ provide.tf
@@ -1,9 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.0"
+provider "google" {
+
 }
